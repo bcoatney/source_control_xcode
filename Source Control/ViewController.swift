aa52838a25174af7e843190656f8b6a838adceab
Ben Coatney <coatneyben@gmail.com>
2021-05-13 14:01:45 -0400

    Initial commit

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+        
+    }
 
 }
 
